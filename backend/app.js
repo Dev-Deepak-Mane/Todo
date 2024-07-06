fa4fbf109a39aa1950cff72a2bb7656da0fda23f
devdeepakmane <devdeepakmane@gmail.com>
2024-07-06 15:52:26 +0530

    added env

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,23 @@ app.use(cookieParser());
 //     credentials: true,
 //   })
 // );
-app.use(cors({ origin: "https://todo-lime-two.vercel.app" }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(
+  cors({
+    origin: ["https://todo-lime-two.vercel.app", "*"],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+//app.use(cors({ origin: ["https://todo-lime-two.vercel.app", "*"] }));
 // Using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
