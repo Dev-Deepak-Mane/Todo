b02c604bae7a98f5ddb9bf1f14a4b59fde522e42
devdeepakmane <devdeepakmane@gmail.com>
2024-07-06 16:15:15 +0530

    added env

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,41 @@
-import { app } from "./app.js";
+import express from "express";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 import { connectDB } from "./data/database.js";
+export const app = express();
+
+config({
+  path: "./data/config.env",
+});
+
+// Using Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(
+  cors({
+    origin: ["https://todo-lime-two.vercel.app/", "*"],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+app.get("/", (req, res) => {
+  res.send("Nice working");
+});
 
 connectDB();
 
@@ -7,5 +43,7 @@ app.listen(process.env.PORT, () => {
   console.log(
     `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
   );
-  return `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`;
 });
+
+// Using Error Middleware
+app.use(errorMiddleware);
