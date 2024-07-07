385637b5220869b0b264270ad8eb61c80509421b
devdeepakmane <devdeepakmane@gmail.com>
2024-07-07 14:52:19 +0530

    new link added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: ["https://todo-dun-ten.vercel.app", "*"],
+    origin: ["https://todo-gold-nine-10.vercel.app", "*"],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     allowedHeaders: "Content-Type, Authorization",
@@ -52,53 +52,3 @@ app.use(errorMiddleware);
 
 // Export the Express app
 export default app;
-
-// import express from "express";
-// import userRouter from "./routes/user.js";
-// import taskRouter from "./routes/task.js";
-// import { config } from "dotenv";
-// import cookieParser from "cookie-parser";
-// import { errorMiddleware } from "./middlewares/error.js";
-// import cors from "cors";
-// import { connectDB } from "./data/database.js";
-
-// // Initialize the app
-// const app = express();
-
-// config({
-//   path: "./data/config.env",
-// });
-
-// // Using Middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
-// app.use(
-//   cors({
-//     origin: ["https://todo-dun-ten.vercel.app", "*"],
-//     credentials: true,
-//     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-//     allowedHeaders: "Content-Type, Authorization",
-//   })
-// );
-
-// // Using routes
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/task", taskRouter);
-// app.get("*", (req, res) => {
-//   res.send("Welcome to todoApp");
-// });
-
-// // Connect to database
-// connectDB();
-
-// // Using Error Middleware
-// app.use(errorMiddleware);
-
-// // Export the Express app
-// export default app;
