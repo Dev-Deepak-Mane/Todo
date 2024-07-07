c6b423cf6e71b61e41181e9872aeab20ad2f5d3e
devdeepakmane <devdeepakmane@gmail.com>
2024-07-07 11:15:43 +0530

    update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -11,7 +11,7 @@ export const isAuthenticated = async (req, res, next) => {
     });
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+  console.log(decoded?._id);
   req.user = await User.findById(decoded._id);
   next();
 };
